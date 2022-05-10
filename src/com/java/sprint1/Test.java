50a6de6614760faa87333509faaf645fbade2062
jamnova12 <jamnova12@gmail.com>
2022-05-09 22:28:00 -0500

    fer made this

--- src/com/java/sprint1/Test.java
+++ src/com/java/sprint1/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Testing GitHub");
+        System.out.println("fernando wrote this :)");
 
     }
 }
