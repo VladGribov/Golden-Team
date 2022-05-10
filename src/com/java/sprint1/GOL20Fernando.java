fcbcc370f3d44c5ad1096286117ea93ad97cc431
jamnova12 <jamnova12@gmail.com>
2022-05-09 22:33:03 -0500

    Fer gol20

---
+++ src/com/java/sprint1/GOL20Fernando.java
@@ -0,0 +1,11 @@
+package com.java.sprint1;
+// Given 2 int values greater than 0,
+// return whichever value is nearest to 21 without going over.
+// Return 0 if they both go over.
+public class GOL20Fernando {
+    public static void main(String[] args) {
+
+        System.out.println("working on it");
+
+    }
+}
