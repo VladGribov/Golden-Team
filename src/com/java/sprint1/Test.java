10da50f748473768d5b86d4b309fac073c7bd950
beckyabildaeva <beckyabildaeva@Beckys-MBP.attlocal.net>
2022-05-09 22:10:28 -0500

    added a line

--- src/com/java/sprint1/Test.java
+++ src/com/java/sprint1/Test.java
@@ -3,5 +3,7 @@ package com.java.sprint1;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Testing GitHub");
+
     }
 }
