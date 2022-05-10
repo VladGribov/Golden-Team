a3514915001607c713c949f3bf0c2c635804c4ef
jamnova12 <jamnova12@gmail.com>
2022-05-10 15:23:57 -0500

    fer did this

--- src/com/java/sprint1/GOL20Fer.java
+++ src/com/java/sprint1/GOL20Fer.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 // Given 2 int values greater than 0,
 // return whichever value is nearest to 21 without going over.
 // Return 0 if they both go over.
-public class GOL20Fernando {
+public class GOL20Fer {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter two digits");
         System.out.println(max(sc.nextInt(), sc.nextInt()));
-
 
     }
 
