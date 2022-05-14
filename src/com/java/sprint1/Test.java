397b57da4dc2b0a743233727d2c42a1372d0c20a
BOBAgfull <vova1998@gmail.com>
2022-05-13 19:15:32 -0700

    Removed Errors

--- src/com/java/sprint1/Test.java
+++ src/com/java/sprint1/Test.java
@@ -4,19 +4,15 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Testing GitHub");
-<<<<<<< HEAD
         System.out.println("fernando wrote this :)");
         System.out.println("Liz testing git");
         System.out.println("Aigul!");
         System.out.println("Hello Golden Team!");
-=======
 
         System.out.println("Aigul!");
 
         System.out.println("fernando wrote this :)");
         System.out.println("Liz testing git");
-
->>>>>>> b54356b1e250f5352227c2c509cfa1c43de77ebe
 
     }
 }
