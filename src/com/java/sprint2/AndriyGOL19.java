98ed4178628d81b4ca5ed1410653fa1f49168654
andriyko16 <humenyuka@gmail.com>
2022-05-15 11:11:15 -0500

    comp

--- src/com/java/sprint2/AndriyGOL19.java
+++ src/com/java/sprint2/AndriyGOL19.java
@@ -1,10 +1,18 @@
 package com.java.sprint2;
 
+import java.util.Locale;
+
 public class AndriyGOL19 {
-    public static void main(String[] args) {
-        String result = "";
+    public String stringYak(String str) {
+        String temp = "";
 
-
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+                i = i + 2;
+            } else {
+                temp += str.charAt(i);
+            }
+        }
+        return temp;
     }
-
 }
