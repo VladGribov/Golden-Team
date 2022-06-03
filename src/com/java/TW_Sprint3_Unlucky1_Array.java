e472ca6908f09a814ff73af82a4f2270e0dfe0b2
VladimirBa <vldmr510@gmail.com>
2022-06-03 13:31:04 -0400

    New tasks solutions added as of 06.03.22

---
+++ src/com/java/TW_Sprint3_Unlucky1_Array.java
@@ -0,0 +1,30 @@
+package com.java;
+/*We'll say that a 1 immediately followed by a 3 in an array is an "unlucky" 1.
+Return true if the given array contains an unlucky 1 in the first 2 or last 2 positions in the array.*/
+import java.util.Scanner;
+
+public class TW_Sprint3_Unlucky1_Array {
+    public static void main(String[] args) {
+        int size;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of the elements in the Array");
+        size = sc.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Enter the Array elements");
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(unlucky1(nums));
+    }
+
+    public static boolean unlucky1(int[] nums) {
+        if (nums.length < 2) {
+            return false;
+        } else if ((nums[0] == 1) && (nums[1] == 3) || (nums[1] == 1) && (nums[2] == 3) ||
+                (nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/com/java/test/TW_Sprint3_FrontPiece_Array.java
@@ -0,0 +1,30 @@
+package com.java.test;
+/*Given an int array of any length, return a new array of its first 2 elements.
+If the array is smaller than length 2, use whatever elements are present.*/
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TW_Sprint3_FrontPiece_Array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the amount of the elements in the Array");
+        int size = sc.nextInt();
+        int[] nums = new int[size];
+        System.out.println("Enter the Array elements");
+        for (int i = 0; i < size; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(frontPiece(nums)));
+    }
+    public static int[] frontPiece(int[] nums) {
+        int[] newNums = new int[2];
+        if (nums.length >= 2) {
+            newNums = new int[]{nums[0], nums[1]};
+        } else if (nums.length == 0) {
+            newNums = new int[]{};
+        } else {
+            newNums = new int[]{nums[0]};
+        }
+        return newNums;
+    }
+}
