bf8a6d3e76bb4c9f6ffccbaeec42ce295727ce35
BOBAgfull <vova1998@gmail.com>
2022-06-21 19:34:38 -0700

    Adding instructions for MapAB4_Vladimir

--- src/com/java/sprint4/MapAB4_Vladimir.java
+++ src/com/java/sprint4/MapAB4_Vladimir.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+/*
+Modify and return the given map as follows: if the keys "a" and "b" have values that have different lengths, then set "c" to have the longer value. If the values exist and have the same length, change them both to the empty string in the map.
+https://codingbat.com/prob/p136950
+ */
 public class MapAB4_Vladimir {
     public Map<String, String> mapAB4(Map<String, String> map) {
         if(!map.containsKey("a") || !map.containsKey("b")){
